6eabb3dada6e5b1840241803aa0364f0bcf0a6c7
Azudeus <blitzthunder68@yahoo.com>
2017-01-25 06:09:33 +0700

    fixed image for better result

--- projector.c
+++ projector.c
@@ -6,6 +6,10 @@
  #include <sys/mman.h>
  #include <sys/ioctl.h>
 
+#define arrwidth 1200
+#define arrheight 650
+#define TIMEDELAY 1
+
  int main()
  {
      int fbfd = 0;
@@ -15,6 +19,16 @@
      char *fbp = 0;
      int x = 0, y = 0;
      long int location = 0;
+
+    static int mat[arrwidth*arrheight*3];
+
+    char * line = NULL;
+    size_t len = 3;
+    ssize_t read;
+    int i=0;
+    int dy=0;
+
+
 
      // Open the file for reading and writing
      fbfd = open("/dev/fb0", O_RDWR);
@@ -50,43 +64,42 @@
      }
      printf("The framebuffer device was mapped to memory successfully.\n");
 
-     x = 50; y = 50;       // Where we are going to put the pixel
-     int xlast = 700; 
-     int ylast=1300;
-     int lx = xlast-x;
-     int ly = ylast-y;
-
-     // Figure out where in memory to put the pixel     
-
-#define arrwidth 1200
-#define arrheight 650
-
-
-static int mat[arrwidth*arrheight*3];
-
-    char * line = NULL;
-    size_t len = 3;
-    ssize_t read;
-    int i=0;
-
-    int val;
-
+     // Open file 
     FILE *fp = fopen("text.ppm", "r");
     if (fp == NULL)
         exit(EXIT_FAILURE);
 
     while ((read = getline(&line, &len, fp)) != -1) {
-        val = atoi(line);
-        mat[i]=val;
+        mat[i] = atoi(line);
         i++;
     }
     fclose(fp);
 
-  i=0;
-  int dy;
-    for(dy = 0 ; dy < 100; dy++){
-     for (y = 100; y < 750; y++){
-         for (x = 50; x < 1250; x++) {
+    // Fill with black color
+     for (y = 10; y < vinfo.yres-10; y++){
+         for (x = 20; x < vinfo.xres-20; x++) {
+             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+                        (y+vinfo.yoffset) * finfo.line_length;
+
+             if (vinfo.bits_per_pixel == 32) {
+                 *(fbp + location) = 0;
+                 *(fbp + location+1) = 0;
+                 *(fbp + location+2) = 0;
+                 *(fbp + location+3) = 0;
+             } else  { //assume 16bpp
+                 int b = 0;
+                 int g = 0;     // A little green
+                 int r = 0;    // A lot of red
+                 unsigned short int t = r<<11 | g << 5 | b;
+                 *((unsigned short int*)(fbp + location)) = t;
+             }
+         }
+     }
+
+    // Project Image to framebuffer
+    for(dy = 0 ; dy < 480; dy++){
+     for (y = 500; y < 750; y++){
+         for (x = 260; x < 991; x++) {
              location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                         (y+vinfo.yoffset-dy) * finfo.line_length;
 
@@ -96,15 +109,18 @@ static int mat[arrwidth*arrheight*3];
                  *(fbp + location+2) = mat[3*i+2];
                  *(fbp + location+3) = 0;
              } else  { //assume 16bpp
-                 int b = 255;
-                 int g = 255;     // A little green
-                 int r = 255;    // A lot of red
+                 int b = 0;
+                 int g = 0;     // A little green
+                 int r = 0;    // A lot of red
                  unsigned short int t = r<<11 | g << 5 | b;
                  *((unsigned short int*)(fbp + location)) = t;
              }
               i++;
          }
+       usleep(1);
+
      }
+     // Fill animation leftover with black
     for (x = 50; x < 1250; x++) {
         location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                     (y+vinfo.yoffset-dy) * finfo.line_length;
@@ -115,9 +131,9 @@ static int mat[arrwidth*arrheight*3];
              *(fbp + location+2) = 0;
              *(fbp + location+3) = 0;
          } else  { //assume 16bpp
-             int b = 255;
-             int g = 255;     // A little green
-             int r = 255;    // A lot of red
+             int b = 0;
+             int g = 0;     // A little green
+             int r = 0;    // A lot of red
              unsigned short int t = r<<11 | g << 5 | b;
              *((unsigned short int*)(fbp + location)) = t;
          }
